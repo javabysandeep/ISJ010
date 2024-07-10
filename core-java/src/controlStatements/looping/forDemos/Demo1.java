1fe5ba808ac66a08c8ddaf1b263906e7a6419acd
JavaBySandeep <42215856+javabysandeep@users.noreply.github.com>
2024-07-10 10:09:30 +0530

    command line arguments

--- core-java/src/controlStatements/looping/forDemos/Demo1.java
+++ core-java/src/controlStatements/looping/forDemos/Demo1.java
@@ -1,13 +1,9 @@
 package controlStatements.looping.forDemos;
 
 public class Demo1 {
-    public static void main(String[] args) {
-        System.out.println(1);
-        System.out.println(2);
-        System.out.println(3);
-        System.out.println(4);
-        System.out.println(5);
-        System.out.println(6);
-        System.out.println(".........");
+    public static void main(String[] array) {
+       for(String str : array){
+           System.out.println(str);
+       }
     }
 }
