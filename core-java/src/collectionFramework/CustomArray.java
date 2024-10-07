30ff46dae74e285aade97fa25aab3e86500e9547
JavaBySandeep <42215856+javabysandeep@users.noreply.github.com>
2024-10-07 09:55:25 +0530

    collection 1

---
+++ core-java/src/collectionFramework/CustomArray.java
@@ -0,0 +1,43 @@
+package collectionFramework;
+
+import java.util.Arrays;
+
+public class CustomArray {
+
+    Object[] values;
+    int capacity = 10;
+    int length = 0;
+
+    public CustomArray() {
+        values = new Object[capacity];
+    }
+
+    public void add(Object object) {
+        if (length < capacity) {
+            values[length++] = object;
+        } else {
+            capacity = capacity * 2;
+            Object[] temp = new Object[capacity];
+            for (int index = 0; index < values.length; index++) {
+                temp[index] = values[index];
+            }
+            temp[length++] = object;
+            values = temp;
+        }
+    }
+
+    public void printValues() {
+        for (int index = 0; index < length; index++) {
+            System.out.print(values[index] + " ");
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "CustomArray{" +
+                "values=" + Arrays.toString(values) +
+                ", capacity=" + capacity +
+                ", length=" + length +
+                '}';
+    }
+}
---
+++ core-java/src/collectionFramework/Demo1.java
@@ -0,0 +1,18 @@
+package collectionFramework;
+
+import java.util.ArrayList;
+
+public class Demo1 {
+    public static void main(String[] args) {
+        CustomArray customArray = new CustomArray();
+        customArray.add(10);
+        customArray.add(20);
+        customArray.add(30);
+        customArray.add(40);
+        customArray.add(10.40f);
+        customArray.add(true);
+        customArray.add("abc");
+        customArray.printValues();
+
+    }
+}
