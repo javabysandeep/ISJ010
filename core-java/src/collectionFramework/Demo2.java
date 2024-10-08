7fe8bef84c28bf439a9dc5bef8a57110e06d8a9d
JavaBySandeep <42215856+javabysandeep@users.noreply.github.com>
2024-10-08 10:04:35 +0530

    collection 2

---
+++ core-java/src/collectionFramework/Demo2.java
@@ -0,0 +1,31 @@
+package collectionFramework;
+
+import java.util.Iterator;
+
+public class Demo2 {
+    public static void main(String[] args) {
+        class A implements Iterable {
+
+            @Override
+            public Iterator iterator() {
+                Iterator iterator = new Iterator() {
+                    @Override
+                    public boolean hasNext() {
+                        return false;
+                    }
+
+                    @Override
+                    public Object next() {
+                        return null;
+                    }
+                };
+                return iterator;
+            }
+        }
+        A a = new A();
+        Iterator iterator = a.iterator();
+        System.out.println(iterator.hasNext());
+        System.out.println(iterator.next());
+
+    }
+}
