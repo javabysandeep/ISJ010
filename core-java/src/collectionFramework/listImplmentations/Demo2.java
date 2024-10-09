f80f291127f2df58e6377b0ef06d1ccb55c465c6
JavaBySandeep <42215856+javabysandeep@users.noreply.github.com>
2024-10-09 10:17:50 +0530

    collection 3

---
+++ core-java/src/collectionFramework/listImplmentations/Demo2.java
@@ -0,0 +1,24 @@
+package collectionFramework.listImplmentations;
+
+import java.util.ArrayList;
+
+public class Demo2 {
+    public static void main(String[] args) {
+        ArrayList arrayList = new ArrayList();
+        arrayList.add(100);
+        arrayList.add(100);
+        arrayList.add(null);
+        arrayList.add(null);
+        arrayList.add("abc");
+        arrayList.add("abc");
+        arrayList.add(10.5f);
+        arrayList.add(true);
+        System.out.println(arrayList);
+
+        ArrayList arrayList2 = new ArrayList<>();
+        arrayList2.addAll(arrayList);
+        arrayList2.add("new list");
+        System.out.println(arrayList2);
+
+    }
+}
---
+++ core-java/src/collectionFramework/listImplmentations/Demo3.java
@@ -0,0 +1,24 @@
+package collectionFramework.listImplmentations;
+
+import java.util.ArrayList;
+
+public class Demo3 {
+    public static void main(String[] args) {
+        ArrayList arrayList = new ArrayList();
+        arrayList.add(100);
+        arrayList.add(100);
+        arrayList.add(null);
+        arrayList.add(null);
+        arrayList.add("abc");
+        arrayList.add("abc");
+        arrayList.add(10.5f);
+        arrayList.add(true);
+        System.out.println("Before removal");
+        System.out.println(arrayList);
+
+        arrayList.remove(true);
+        System.out.println("after removal");
+        System.out.println(arrayList);
+
+    }
+}
